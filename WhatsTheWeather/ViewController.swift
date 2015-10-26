86cc30325861e1cbcaa2273ab32e0ce362f67378
Sachin Virdy <svirdy1@gmail.com>
2015-10-25 17:26:52 -0700

    first layout

--- WhatsTheWeather/ViewController.swift
+++ WhatsTheWeather/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var userCity: UITextField!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+    }
 
+    @IBOutlet var resultLabel: UILabel!
+    
+    func showError() {
+        resultLabel.text = "Was not able to find weather for " + userCity.text + ". Please try again."
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest")
+        if url != nil {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: {(data, response, error) -> Void in
+                
+                var urlError = false
+                
+                if error != nil {
+                    
+                } else {
+                    urlError = true
+                }
+                
+                if urlError == true {
+                    self.showError()
+                }
+            })
+        } else {
+            showError()
+        }
     }
 
     override func didReceiveMemoryWarning() {
